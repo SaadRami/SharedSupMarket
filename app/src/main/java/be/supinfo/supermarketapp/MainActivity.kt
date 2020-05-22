30d0265941d8dd72ff8c65e216d1e3ae95025d72
saad <sr.saadrami@gmail.com>
2020-05-22 05:44:54 +0200

    commit aden

--- app/src/main/java/be/supinfo/supermarketapp/MainActivity.kt
+++ app/src/main/java/be/supinfo/supermarketapp/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_nav)
         setSupportActionBar(toolbar)
 
 
